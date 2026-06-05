7a11d65af1edc5d74720afa0c5f16a433d2e5600
LandinOddo32 <lrichoux09@gmail.com>
2026-06-05 15:31:19 +0000

    completed data shapes project

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
   // Your code goes here
-
+  $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
 });
+
+$("#die").on("click", handleClick);
+
+function makeDot() {}
+
+function rollDie(dieID) {
+  var randomNum = Math.ceil(Math.random() * 6);
+console.log(randomNum);;
+}
+
+function handleClick() {
+  rollDie("#die");
+} 
+
